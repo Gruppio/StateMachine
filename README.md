2a123a6544e9ef2be101894a587fdc43b5860ccc
Michele Gruppioni <gruppiofigo@hotmail.it>
2021-03-22 01:15:00 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
 # StateMachine
 Create State Machines with class
+
+## Advantages
+Create a State Machine has never been easier!
+There are just 2 components `StateMachine` and `State`.
+There are some state's decorators that will help develop your applications logic.
+
+## StateMachine
+The StateMachine is the orchestrator of the states.
+It has a generic `Context` parameter that allows to provide some informations or dependencies to the states.
+
+## State
+Subclass the `State` class and override the `handleState` method.
+
+## State Decorators
+
+### Delay
+The `State`'s `delay` method will return a new `DelayedState` that will switch to the new state after a period of time
+
+### Throttle
+The `State`'s `throttle` method will return a new `ThrottledState` call the `handleState` max once every period of time
+
+### Recovery
+The `State`'s `recoveryAfter` method will switch to a recoveryState if the state is still running after a period of time
+
+## Example
+```c
+// This example creates a StateMachine with 3 states
+
+// Include the StateMachine library found here :
+// https://github.com/Gruppio/StateMachine
+
+#include <Arduino.h>
+#include <StateMachine.h>
+
+struct StateContext {
+  bool isCompleted = false;
+};
+
+class FinalState: public State<StateContext*> {
+  void handleState(StateMachine<StateContext*> *stateMachine) {
+    Serial.println("Perform FinalState work");
+    stateMachine->context->isCompleted = true;
+  }
+};
+
+class SecondState: public State<StateContext*> {
+  void handleState(StateMachine<StateContext*> *stateMachine) {
+    Serial.println("Perform SecondState work");
+    // Will call the `handleState` of `FinalState` max once every `100ms`
+    stateMachine->setState(
+        (new FinalState())->throttle(100);
+    );
+  }
+};
+
+class InitialState: public State<StateContext*> {
+  void handleState(StateMachine<StateContext*> *stateMachine) {
+    Serial.println("Perform BeginState work");
+    // Will transition to `SecondState` after 1000ms
+    stateMachine->setState(
+        (new SecondState())->delay(1000);
+    );
+  }
+};
+
+StateContext *stateContext;
+StateMachine<StateContext*> *stateMachine;
+
+void setup() {
+  stateContext = new StateContext();
+  stateMachine = new StateMachine<StateContext*>(stateContext, new InitialState());
+}
+
+void loop() {
+   stateMachine->handleState();
+}
+```
+
+## If you like this project please:
+
+<a href="https://www.buymeacoffee.com/gruppio" target="_blank"><img src="https://raw.githubusercontent.com/Gruppio/Sonoff-Homekit/images/images/buymeacoffee.png" alt="Buy Me A Coffee" width="300" ></a>
+
+## Thank you!
---
+++ src/StateMachine.h
@@ -0,0 +1,186 @@
+//
+//  StateMachine.h
+//  StateMachine
+//
+//  Created by Michele Gruppioni on 21/03/21.
+//
+
+#ifndef StateMachine_h
+#define StateMachine_h
+
+extern unsigned long millis();
+
+template <class Context>
+class State;
+
+template <class Context>
+class ThrottledState;
+
+template <class Context>
+class DelayedState;
+
+template <class Context>
+class RecoveryState;
+
+// StateMachine
+
+template <class Context>
+class StateMachine
+{
+public:
+  Context context;
+  
+private:
+  State<Context> *state;
+  
+public:
+  StateMachine(Context context, State<Context> *initialState);
+  void setState(State<Context> *newState);
+  void handleState();
+};
+
+template <class Context>
+StateMachine<Context>::StateMachine(Context context, State<Context> *initialState) : context(context),
+state(initialState)
+{
+}
+
+template <class Context>
+void StateMachine<Context>::setState(State<Context> *newState)
+{
+  state = newState;
+}
+
+template <class Context>
+void StateMachine<Context>::handleState()
+{
+  state->handleState(this);
+}
+
+// State
+
+template <class Context>
+class State
+{
+public:
+  virtual void handleState(StateMachine<Context> *stateContext) = 0;
+  
+  State<Context>* throttle(unsigned long interval);
+  State<Context>* delay(unsigned long interval);
+  State<Context>* recoveryAfter(unsigned long maxDuration, State<Context> *recoveryState);
+};
+
+template <class Context>
+State<Context>* State<Context>::throttle(unsigned long interval)
+{
+  return new ThrottledState<Context>(interval, this);
+}
+
+template <class Context>
+State<Context>* State<Context>::delay(unsigned long interval)
+{
+  return new DelayedState<Context>(interval, this);
+}
+
+template <class Context>
+State<Context>* State<Context>::recoveryAfter(unsigned long maxDuration, State<Context> *recoveryState)
+{
+  return new RecoveryState<Context>(maxDuration, recoveryState, this);
+}
+
+// ThrottledState
+
+template <typename Context>
+class ThrottledState : public State<Context>
+{
+private:
+  unsigned long interval;
+  State<Context> *nextState;
+  unsigned long lastTimeRunned = 0;
+  
+public:
+  ThrottledState(unsigned long interval, State<Context> *nextState);
+  void handleState(StateMachine<Context> *stateMachine);
+};
+
+template <class Context>
+ThrottledState<Context>::ThrottledState(unsigned long interval, State<Context> *nextState) :
+interval(interval),
+nextState(nextState)
+{
+}
+
+template <class Context>
+void ThrottledState<Context>::handleState(StateMachine<Context> *stateMachine)
+{
+  if (millis() >= lastTimeRunned + interval)
+  {
+    lastTimeRunned = millis();
+    nextState->handleState(stateMachine);
+  }
+}
+
+// DelayedState
+
+template <class Context>
+class DelayedState : public State<Context>
+{
+private:
+  unsigned long delayValue;
+  State<Context> *nextState;
+  unsigned long startTime = millis();
+  
+public:
+  DelayedState(unsigned long delayValue, State<Context> *nextState);
+  void handleState(StateMachine<Context> *stateMachine);
+};
+
+template <class Context>
+DelayedState<Context>::DelayedState(unsigned long delayValue, State<Context> *nextState):
+delayValue(delayValue),
+nextState(nextState) {
+}
+
+template <class Context>
+void DelayedState<Context>::handleState(StateMachine<Context> *stateMachine) {
+  if(millis() > startTime + delayValue) {
+    stateMachine->setState(nextState);
+  }
+}
+
+// RecoveryState
+
+template <class Context>
+class RecoveryState : public State<Context>
+{
+private:
+  unsigned long maxDuration;
+  State<Context> *recoveryState;
+  State<Context> *state;
+  unsigned long startTime = 0;
+  
+public:
+  RecoveryState(unsigned long maxDuration, State<Context> *recoveryState, State<Context> *state);
+  void handleState(StateMachine<Context> *stateMachine);
+};
+
+template <class Context>
+RecoveryState<Context>::RecoveryState(unsigned long maxDuration, State<Context> *recoveryState, State<Context> *state):
+maxDuration(maxDuration),
+recoveryState(recoveryState),
+state(state) {
+}
+
+template <class Context>
+void RecoveryState<Context>::handleState(StateMachine<Context> *stateMachine) {
+  if (startTime == 0)
+    startTime = millis();
+  
+  if (millis() > startTime + maxDuration) {
+    stateMachine->setState(recoveryState);
+  } else {
+    stateMachine->handleState(stateMachine);
+  }
+}
+
+#endif /* StateMachine_h */
