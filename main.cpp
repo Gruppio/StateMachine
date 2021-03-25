6fbbfa43343be222bc2cf502fdd219fa9af50573
Michele Gruppioni <gruppiofigo@hotmail.it>
2021-03-25 01:46:20 +0100

    update main

--- main.cpp
+++ main.cpp
@@ -5,21 +5,35 @@
 //  Created by Michele Gruppioni on 21/03/21.
 //
 
+#include <chrono>
 #include <iostream>
+#include <sys/time.h>
+#include <ctime>
 #include "src/StateMachine.h"
 
 unsigned long millis() {
-  return 0;
+  return (unsigned long)time(nullptr) * 1000 ;
 }
 
 struct StateContext {
   bool isCompleted = false;
 };
 
+class ErrorState: public State<StateContext*> {
+  void handleState(StateMachine<StateContext*> *stateMachine) {
+    if (!stateMachine->context->isCompleted) {
+      std::cout << "Error\n";
+      stateMachine->context->isCompleted = true;
+    }
+  }
+};
+
 class FinalState: public State<StateContext*> {
   void handleState(StateMachine<StateContext*> *stateMachine) {
-    std::cout << "Perform FinalState work\n";
-    stateMachine->context->isCompleted = true;
+    if (!stateMachine->context->isCompleted) {
+      std::cout << "Perform FinalState work\n";
+      stateMachine->context->isCompleted = true;
+    }
   }
 };
 
@@ -33,7 +47,12 @@ class SecondState: public State<StateContext*> {
 class InitialState: public State<StateContext*> {
   void handleState(StateMachine<StateContext*> *stateMachine) {
     std::cout << "Perform BeginState work\n";
-    stateMachine->setState(new SecondState());
+    stateMachine->setState(
+                           (new SecondState())
+                           ->recoveryAfter(3000, new ErrorState(), new ErrorState())
+                           ->throttle(100)
+                           ->delay(1000)
+                           );
   }
 };
 
@@ -41,9 +60,12 @@ int main(int argc, const char * argv[]) {
   StateContext *stateContext = new StateContext();
   StateMachine<StateContext*> *stateMachine = new StateMachine<StateContext*>(stateContext, new InitialState());
   
+//  State<StateContext*> *s = (new InitialState())->delay(1000)->throttle(1000)->recoveryAfter(100, new ErrorState(), new ErrorState());
+//  delete s;
+  
   while (!stateContext->isCompleted) {
     stateMachine->handleState();
   }
-
+  
   return 0;
 }
